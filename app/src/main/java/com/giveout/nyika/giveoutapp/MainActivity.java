42c1a64021d1cd9f4e636edd5a857464aaa9e137
Dineo Senona <dineosenona@Dineos-MacBook-Pro.local>
2018-08-04 12:59:01 +0200

    Initial commit

---
+++ app/src/main/java/com/giveout/nyika/giveoutapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.giveout.nyika.giveoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
